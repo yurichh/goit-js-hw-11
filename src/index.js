c651566e320ba12c290ed9149c560f9315afd977
unknown <velidchukaleksey@gmail.com>
2023-10-08 13:24:08 +0300

    start1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import axios from 'axios';
+// import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+// import axios from 'axios';
+console.log('Hello');
